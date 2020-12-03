4447108da4eec73cb870a4354db364c47a68dd2a
Theodore Pollock <pollock86@yahoo.com>
2020-12-02 20:01:40 -0600

    updated Billing error

---
+++ ServiceDesk/GlobalSuppressions.cs
@@ -0,0 +1,12 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:ServiceDesk.Startup._hostingEnvironment")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:ServiceDesk.Startup._configuration")]
+[assembly: SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Pending>", Scope = "member", Target = "~F:ServiceDesk.Data.SeedData._employee")]
+[assembly: SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Pending>", Scope = "member", Target = "~F:ServiceDesk.Data.SeedData._technicians")]
+[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>", Scope = "member", Target = "~M:ServiceDesk.Startup.Configure(Microsoft.AspNetCore.Builder.IApplicationBuilder,ServiceDesk.Data.ServiceDeskContext,Microsoft.Extensions.Logging.ILogger{ServiceDesk.Startup},Microsoft.AspNetCore.Hosting.IApplicationLifetime,Microsoft.AspNetCore.Identity.UserManager{ServiceDesk.Models.Technician})")]
--- ServiceDesk/Startup.cs
+++ ServiceDesk/Startup.cs
@@ -82,3 +82,5 @@ namespace ServiceDesk
         }
     }
 }
+
+
